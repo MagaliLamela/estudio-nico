f6a449a7b460f7925bcc21d1aa8704e08c514431
magali.lamela <magalilamelamitsumori@gmail.com>
2024-09-26 19:17:48 -0300

    corregimos el mapa

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -64,7 +64,7 @@ const Contacto = () => {
         }
     };
 
-    const iframeSrc = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3283.961095365186!2d-58.3934538236301!3d-34.60514527295386!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccac567612e09%3A0xd3c44ff2ece2f7b5!2sEstudio%20Jur%C3%ADdico!5e0!3m2!1ses-419!2sar!4v1723307247514!5m2!1ses-419!2sar";
+    const iframeSrc = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3283.962798779461!2d-58.39577510321041!3d-34.6051022!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccb12cc97a10d%3A0xf71e3948a36fc54f!2sBerraondo%2C%20Galarza%20%26%20Rocha%20Estudio%20Jur%C3%ADdico!5e0!3m2!1ses-419!2sar!4v1727388922948!5m2!1ses-419!2sar";
 
     return (
         <div className='contacto'>
